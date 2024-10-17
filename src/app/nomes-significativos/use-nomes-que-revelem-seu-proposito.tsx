84b312895f1b83341b226037de3101a952112668
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-10-17 18:11:34 -0300

    Convertando codigo para componente react

--- src/app/nomes-significativos/use-nomes-que-revelem-seu-proposito.tsx
+++ src/app/nomes-significativos/use-nomes-que-revelem-seu-proposito.tsx
@@ -1,37 +1,35 @@
-/*
- * Atenção!
- *
- * Os comentários deste arquivo são para fins de aprendizagem,
- * Sempre evite comentários no código! - Escolha bons nomes que revelem seu proposito.
- *
- */
+export const CodigoRuim: React.FC = () => {
+   // Skills
+   const s = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
 
-// ==================================================================================================
+   // getSkill
+   const gs = (n: string) => s.filter((s) => n === s);
 
-/*
- * # Nome Ruim
- *
- * O nome 's' não significa nada.
- * Se um nome precisa de um comentário, então ele não mostra o proposito.
- * Que tipo de coisa está armazenada na variável 's' ?
- *
- */
+   console.log(gs("Clean Code"));
 
-// Skills
-const s = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
+   return (
+      <ul>
+         <li>
+            <strong>Còdigo Ruim</strong>
+         </li>
+         <li>Se um nome precisa de um comentário, então ele não mostra o proposito.</li>
+         <li>Que tipo de coisa está armazenada na variável {"s"} ?</li>
+         <li>O nome {"s"} não significa nada.</li>
+      </ul>
+   );
+};
 
-// getSkill
-const gs = (n: string) => s.filter((s) => n === s);
-console.log(gs("Clean Code"));
+export const CodigoBom: React.FC = () => {
+   const skills: string[] = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
+   const getSkill = (name: string) => skills.filter((skill) => name === skill);
+   console.log(getSkill("Clean Code"));
 
-// ==================================================================================================
-
-/*
- * # Bom nome
- *
- * Nomes claros mostrando o propósito e facilitando o entendimento.
- *
- */
-const skills = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
-const getSkill = (name: string) => skills.filter((skill) => name === skill);
-console.log(getSkill("Clean Code"));
+   return (
+      <ul>
+         <li>
+            <strong>Código Bom</strong>
+         </li>
+         <li>Nomes claros mostrando o propósito e facilitando o entendimento.</li>
+      </ul>
+   );
+};
