c5adc15e44fec250455278de098519b526b9488d
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-10-17 19:27:57 -0300

    evite informacoes erradas

--- src/app/nomes-significativos/use-nomes-que-revelem-seu-proposito.tsx
+++ src/app/nomes-significativos/use-nomes-que-revelem-seu-proposito.tsx
@@ -1,35 +1,39 @@
 export const CodigoRuim: React.FC = () => {
-   // Skills
-   const s = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
+  // Skills
+  const s = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
 
-   // getSkill
-   const gs = (n: string) => s.filter((s) => n === s);
+  // getSkill
+  const gs = (n: string) => s.filter((s) => n === s);
 
-   console.log(gs("Clean Code"));
+  console.log(gs("Clean Code"));
 
-   return (
-      <ul>
-         <li>
-            <strong>Còdigo Ruim</strong>
-         </li>
-         <li>Se um nome precisa de um comentário, então ele não mostra o proposito.</li>
-         <li>Que tipo de coisa está armazenada na variável {"s"} ?</li>
-         <li>O nome {"s"} não significa nada.</li>
-      </ul>
-   );
+  return (
+    <ul>
+      <li>
+        <strong>Còdigo Ruim</strong>
+      </li>
+      <li>Se um nome precisa de um comentário, então ele não mostra o proposito.</li>
+      <li>Que tipo de coisa está armazenada na variável {"s"} ?</li>
+      <li>O nome {"s"} não significa nada.</li>
+    </ul>
+  );
 };
 
 export const CodigoBom: React.FC = () => {
-   const skills: string[] = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
-   const getSkill = (name: string) => skills.filter((skill) => name === skill);
-   console.log(getSkill("Clean Code"));
+  const skills: string[] = ["TypeScript", "ReactJS", "NextJS", "Clean Code", "TDD"];
+  const getSkill = (name: string) => skills.filter((skill) => name === skill);
+  console.log(getSkill("Clean Code"));
 
-   return (
-      <ul>
-         <li>
-            <strong>Código Bom</strong>
-         </li>
-         <li>Nomes claros mostrando o propósito e facilitando o entendimento.</li>
-      </ul>
-   );
+  return (
+    <ul>
+      <li>
+        <strong>Código Bom</strong>
+      </li>
+      <li>Nomes claros mostrando o propósito e facilitando o entendimento.</li>
+      <li>
+        Obs: utilizei o tipo {"string[]"} na variavel skills, porém, o typescript ofereçe suporte a inferência de tipos, por
+        tanto, neste caso prefira não adicionar o tipo, deixe que o typescript infira o tipo automaticamente.
+      </li>
+    </ul>
+  );
 };
